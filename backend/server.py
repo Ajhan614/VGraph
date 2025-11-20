c4e6abab387ef7137e0a8855217903941b486553
Ajhan614 <dlya.uchoby.1try@gmail.com>
2025-11-21 00:48:30 +0500

    backend added

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/data')
+def get_time():
+    return jsonify({
+        "Name": "geek",
+        "Age": "22",
+        "Date": datetime.datetime.now().isoformat(),
+        "programming": "python"
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
