91488f660b22e1e1417122908d33de9cf55875f9
Ajhan614 <dlya.uchoby.1try@gmail.com>
2026-02-15 01:11:44 +0500

    Not always has orthopath but stable

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import networkx as nx
 import pydot
+import os
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='build')
 CORS(app)
 
-
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve_react(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+    
 @app.route('/calculate_coordinates', methods=['POST'])
 def calculate_coordinates():
     try:       
