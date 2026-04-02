098e552726fcd77a5b163f19372cf311d8445c97
Ajhan614 <dlya.uchoby.1try@gmail.com>
2026-04-02 22:36:06 +0500

    build

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Navbar.css';
+import logo from '../../assets/eye.png';
+import { Link } from 'react-router-dom'; // Import Link here
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" className="logo" />
+      <h1>Graph Visualizer</h1>
+      <ul className="nav-link">
+        <li><Link to="/">Главная</Link></li>
+        <li><Link to="/settings">Настройки</Link></li>
+        <li><Link to="/documentation">Документация</Link></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
